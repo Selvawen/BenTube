96cd8a7dd13bc73840ef0558f00c90f49c849d8c
Selvawen <benanderson370@gmail.com>
2024-12-28 05:59:56 -0800

    project added

---
+++ src/utils/constants.js
@@ -0,0 +1,42 @@
+import MusicNoteIcon from "@mui/icons-material/MusicNote";
+import HomeIcon from "@mui/icons-material/Home";
+import CodeIcon from "@mui/icons-material/Code";
+import OndemandVideoIcon from "@mui/icons-material/OndemandVideo";
+import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
+import LiveTvIcon from "@mui/icons-material/LiveTv";
+import SchoolIcon from "@mui/icons-material/School";
+import FaceRetouchingNaturalIcon from "@mui/icons-material/FaceRetouchingNatural";
+import CheckroomIcon from "@mui/icons-material/Checkroom";
+import GraphicEqIcon from "@mui/icons-material/GraphicEq";
+import TheaterComedyIcon from "@mui/icons-material/TheaterComedy";
+import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
+import DeveloperModeIcon from "@mui/icons-material/DeveloperMode";
+import LogoImage from "../assets/logo.png";
+import demoThumbnailImage from "../assets/demo-thumbnail.png";
+import demoProfileImage from "../assets/demo-profile.png";
+
+export const logo = LogoImage;
+
+export const categories = [
+  { name: "New", icon: <HomeIcon /> },
+  { name: "Comedy", icon: <TheaterComedyIcon /> },
+  { name: "Coding", icon: <CodeIcon /> },
+  { name: "Education", icon: <SchoolIcon /> },
+  { name: "Podcast", icon: <GraphicEqIcon /> },
+  { name: "Movie", icon: <OndemandVideoIcon /> },
+  { name: "Gaming", icon: <SportsEsportsIcon /> },
+  { name: "Music", icon: <MusicNoteIcon /> },
+  { name: "Live", icon: <LiveTvIcon /> },
+  { name: "Sport", icon: <FitnessCenterIcon /> },
+  { name: "Fashion", icon: <CheckroomIcon /> },
+  { name: "Beauty", icon: <FaceRetouchingNaturalIcon /> },
+  { name: "Gym", icon: <FitnessCenterIcon /> },
+  { name: "Blockchain", icon: <DeveloperModeIcon /> },
+];
+
+export const demoThumbnailUrl = demoThumbnailImage;
+export const demoChannelUrl = "/channel/UC9-y-6csu5WGm29I7JiwpnA";
+export const demoVideoUrl = "/video/BBJa32lCaaY";
+export const demoChannelTitle = "Awesome Channel";
+export const demoVideoTitle = "A Random YouTube Video";
+export const demoProfilePicture = demoProfileImage;
